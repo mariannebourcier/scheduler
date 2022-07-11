af4cd758d3fe8dfd15c318b3e17eb351fd5b3e07
mariannebourcier <marianne.bourcier@gmail.com>
2022-07-11 14:15:44 -0600

    Create classname object for day list conditional classes

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,45 @@
 import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
 
 export default function DayListItem(props) {
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots
+  })
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li 
+    className = {dayClass}
+    onClick = {() => props.setDay(props.name)}
+    >
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{props.spots} spots remaining</h3>
     </li>
+
   );
 }
+  // return (
+  //   <li onClick={() => props.setDay(props.name)}>
+  //     <h2 className="text--regular">{props.name}</h2>
+  //     <h3 className="text--light">{props.spots} spots remaining</h3>
+  //   </li>
+  // );
+
+
+// export default function Button(props) {
+//   const buttonClass = classNames("button", {
+//     "button--confirm": props.confirm,
+//     "button--danger": props.danger
+//   });
+
+//   return (
+//     <button
+//       className={buttonClass}
+//       onClick={props.onClick}
+//       disabled={props.disabled}
+//     >
+//       {props.children}
+//     </button>
+//   );
+// }
