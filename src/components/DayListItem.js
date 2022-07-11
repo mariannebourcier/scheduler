45dfe02524d48534da765c6499b5f75c35dcc9af
mariannebourcier <marianne.bourcier@gmail.com>
2022-07-11 14:27:25 -0600

    Create function for daylist for spots remaining

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,8 +2,21 @@ import React from "react";
 import "components/DayListItem.scss";
 import classNames from "classnames";
 
+const formatSpots = spots => {
+  if (!spots) {
+    return `no spots remaining`;
+  }
+
+  if (spots === 1) {
+    return `${spots} spot remaining`;
+  }
+
+  return `${spots} spots remaining`
+}
 
 export default function DayListItem(props) {
+  const spotsLeft = formatSpots(props.spots);
+
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": !props.spots
@@ -14,7 +27,7 @@ export default function DayListItem(props) {
     onClick = {() => props.setDay(props.name)}
     >
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{spotsLeft}</h3>
     </li>
 
   );
