5caefec690dd0a0659fff4153efee6f739bec39d
mariannebourcier <marianne.bourcier@gmail.com>
2022-07-14 15:46:29 -0600

    Create empty component, add css and story

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,7 @@ import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header.js";
+import Empty from "components/Appointment/Empty.js";
 
 
 storiesOf("Button", module)
@@ -169,3 +170,8 @@ storiesOf("Button", module)
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
+
+
+  //empty
+  storiesOf("Appointment", module)
+  .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
