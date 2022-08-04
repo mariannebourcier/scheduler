360e3d434b9341afa06a7a9afbc9db9f53b8b7ae
mariannebourcier <marianne.bourcier@gmail.com>
2022-08-04 14:39:13 -0600

    Remove comments

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -117,41 +117,5 @@ describe("Form", () => {
   
     expect(onCancel).toHaveBeenCalledTimes(1);
   });
-  // it("calls onSave function when the name and interviewer is defined", () => {
-  //   /* 1. Create the mock onSave function */
-  //   const onSave = jest.fn();
-  
-  //   /* 2. Render the Form with interviewers, student name and the onSave mock function passed as an onSave prop */
-  //   const { getByText, queryByText } = render(
-  //     <Form
-  //       interviewers={interviewers}
-  //       onSave={onSave}
-  //       name="Lydia Miller-Jones"
-  //       interviewer={interviewers[0]}
-  //     />
-  //   );
-  
-  //   /* 3. Click the save button */
-  //   fireEvent.click(getByText("Save"));
-  
-  //   expect(queryByText(/student name cannot be blank/i)).toBeNull();
-  //   expect(queryByText(/please select an interviewer/i)).toBeNull();
-  //   expect(onSave).toHaveBeenCalledTimes(1);
-  //   expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
-  // });
-
-  // it("submits the name entered by the user", () => {
-  //   const onSave = jest.fn();
-  //   const { getByText, getByPlaceholderText } = render(
-  //     <Form interviewers={interviewers} onSave={onSave} interviewer={1} />
-  //   );
-  
-  //   const input = getByPlaceholderText("Enter Student Name");
-  
-  //   fireEvent.change(input, { target: { value: "Lydia Miller-Jones" } });
-  //   fireEvent.click(getByText("Save"));
-  
-  //   expect(onSave).toHaveBeenCalledTimes(1);
-  //   expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
-  // });
+ 
 });
