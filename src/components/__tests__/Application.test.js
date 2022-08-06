8efc5c1b2d7d08e12d33479b8044cdba00826949
mariannebourcier <marianne.bourcier@gmail.com>
2022-08-06 14:04:47 -0600

    Add scoping for containers in application test file

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import { render, cleanup,  waitForElement, fireEvent, getByText, getByAltText, getAllByTestId, prettyDOM, getByPlaceholderText } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -17,3 +17,26 @@ it("defaults to Monday and changes the schedule when a new day is selected", asy
 
   });
 
+it ("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+  const { container } = render(<Application />);
+  
+  const appointments = getAllByTestId(container, "appointment");
+  console.log(prettyDOM(appointments));
+
+  const appointment = getAllByTestId(container, "appointment")[0];
+  console.log(prettyDOM(appointment));
+
+  await waitForElement(() => getByText(container, "Archie Cohen"));
+
+  fireEvent.click(getByAltText(appointment, "Add"));
+
+  fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+   target: { value: "Lydia Miller-Jones" }
+    });
+  fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+  fireEvent.click(getByText(appointment, "Save"));
+
+  console.log(prettyDOM(container));
+
+});
