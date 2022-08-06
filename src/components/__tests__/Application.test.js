2d019050bc170ab153ad86fd602b9584b086bbfd
mariannebourcier <marianne.bourcier@gmail.com>
2022-08-06 14:08:44 -0600

    Remove some console logs, refactor test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -20,23 +20,26 @@ it("defaults to Monday and changes the schedule when a new day is selected", asy
 it ("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
   const { container } = render(<Application />);
   
+  await waitForElement(() => getByText(container, "Archie Cohen"));
+
+
   const appointments = getAllByTestId(container, "appointment");
   console.log(prettyDOM(appointments));
 
-  const appointment = getAllByTestId(container, "appointment")[0];
-  console.log(prettyDOM(appointment));
+  //const appointment = getAllByTestId(container, "appointment")[0];
+  const appointment = appointments[0];
 
-  await waitForElement(() => getByText(container, "Archie Cohen"));
 
   fireEvent.click(getByAltText(appointment, "Add"));
 
   fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
    target: { value: "Lydia Miller-Jones" }
-    });
+  });
+  
   fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
 
   fireEvent.click(getByText(appointment, "Save"));
 
-  console.log(prettyDOM(container));
+  console.log(prettyDOM(appointment));
 
 });
