76d806ebc0ee5acf4f721fc872714ec215018921
mariannebourcier <marianne.bourcier@gmail.com>
2022-08-09 13:50:50 -0600

    Install Cypress and create cypress json file

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup,  waitForElement, fireEvent, getByText, getByAltText, getAllByTestId, prettyDOM, getByPlaceholderText, queryByText, queryByAltText } from "@testing-library/react";
+import { render, cleanup,  waitForElement, fireEvent, getByText, getByAltText, getAllByTestId, prettyDOM, getByPlaceholderText, queryByText, queryByAltText, queryAllByAltText } from "@testing-library/react";
 
 import Application from "components/Application";
 import axios from "__mocks__/axios";
